033818668f38be096f9c0ebe9d099b3e24557b18
lee-aaron <sphinxer34@yahoo.com>
2016-11-14 19:25:32 -0800

    Implemented search bar activity Added Google AdMobs Firebase

--- app/src/main/java/com/weebly/codelet/codelet/App.java
+++ app/src/main/java/com/weebly/codelet/codelet/App.java
@@ -8,5 +8,6 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
+        
     }
 }
--- app/src/main/java/com/weebly/codelet/codelet/MainActivity.java
+++ app/src/main/java/com/weebly/codelet/codelet/MainActivity.java
@@ -2,13 +2,81 @@ package com.weebly.codelet.codelet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] items;
+    ArrayList<String> listItems;
+    ArrayAdapter<String> adapter;
+    ListView listView;
+    EditText editText;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        listView=(ListView)findViewById(R.id.listview);
+        editText=(EditText)findViewById(R.id.txtsearch);
+        initList();
+        editText.addTextChangedListener(new TextWatcher() {
+            int length = 0;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                length=s.toString().length();
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().equals("")) {
+                    initList();
+                }
+                else {
+                    searchItem(s.toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().length() < length) {
+                    initList();
+                    for (String item:items) {
+                        if (!item.toLowerCase().contains(s.toString().toLowerCase())) {
+                            listItems.remove(item);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void searchItem (String txtToSearch) {
+        for (String item:items) {
+            if (!item.toLowerCase().contains(txtToSearch.toString().toLowerCase())) {
+                listItems.remove(item);
+            }
+
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    private void initList()
+    {
+        items=new String[]{"Syntax","Types","Methods","Loops","Arrays"};
+        listItems=new ArrayList<>(Arrays.asList(items));
+        adapter=new ArrayAdapter<String>(this, R.layout.list_item, R.id.txtitem, listItems);
+        listView.setAdapter(adapter);
     }
 }
