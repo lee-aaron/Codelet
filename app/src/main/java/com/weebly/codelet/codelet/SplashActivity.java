1484416212b8b4a2563f2b02eeb4094fa463ef8e
Edward Chau <edwarddchau@gmail.com>
2016-11-20 21:39:37 -0800

    Update SplashActivity.java

--- app/src/main/java/com/weebly/codelet/codelet/SplashActivity.java
+++ app/src/main/java/com/weebly/codelet/codelet/SplashActivity.java
@@ -11,6 +11,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        SystemClock.sleep(4000);
         finish();
     }
 }
