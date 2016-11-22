63a7b478a90c258d9d4f9a9c54c68934416f6414
lee-aaron <sphinxer34@yahoo.com>
2016-11-22 13:54:52 -0800

    Made background

--- app/src/main/java/com/weebly/codelet/codelet/MainActivity.java
+++ app/src/main/java/com/weebly/codelet/codelet/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     private AdView mAdView;
