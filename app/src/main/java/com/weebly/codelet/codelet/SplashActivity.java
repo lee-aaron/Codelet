650365a9d13aa09a9c813148f51a542662c34e9b
lee-aaron <sphinxer34@yahoo.com>
2016-11-21 19:11:29 -0800

    Updated Firebase version and imported class for Splash Screen delay

--- app/src/main/java/com/weebly/codelet/codelet/SplashActivity.java
+++ app/src/main/java/com/weebly/codelet/codelet/SplashActivity.java
@@ -2,6 +2,7 @@ package com.weebly.codelet.codelet;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
