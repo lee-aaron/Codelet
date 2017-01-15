0eaec2b8c700c3c5a6e0b1201d115565df62adf2
edchau <Edward@Edwards-MacBook-Pro.local>
2017-01-15 13:57:18 -0800

    splash

--- app/src/main/java/com/weebly/codelet/codelet/SplashActivity.java
+++ app/src/main/java/com/weebly/codelet/codelet/SplashActivity.java
@@ -1,16 +1,28 @@
 package com.weebly.codelet.codelet;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(1500);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
     }
 }
