984e6ff19dabd28a3f8e5927ac785f15f750cc18
edchau <Edward@Edwards-MacBook-Pro.local>
2017-04-03 10:23:07 -0700

    Arrays Lesson

--- app/src/main/java/com/weebly/codelet/codelet/ArraysFragment.java
+++ app/src/main/java/com/weebly/codelet/codelet/ArraysFragment.java
@@ -2,11 +2,21 @@ package com.weebly.codelet.codelet;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+
+import static android.content.ContentValues.TAG;
 
 public class ArraysFragment extends Fragment{
 
@@ -16,6 +26,30 @@ public class ArraysFragment extends Fragment{
         AdView mAdView = (AdView) result.findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+        ImageView view = (ImageView) result.findViewById(R.id.arrayFragmentImage);
+        Glide.with(this)
+                .load("https://drive.google.com/file/d/0Bx8Vch5mi4mwQUY3Zk5LcVVGQVU/view")
+                .placeholder(R.drawable.loading)
+                .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        if(e != null){
+                            Log.d(TAG, "Listener onException: " + e.toString());
+                        }
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        Log.d(TAG, "onResourceReady with resource = " + resource);
+                        Log.d(TAG, "onResourceReady from memory cache = " + isFromMemoryCache);
+                        return false;
+                    }
+                })
+                .error(R.drawable.noimage)
+                .into(view);
+
         return result;
     }
 
